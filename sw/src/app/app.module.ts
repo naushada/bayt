c461114d96ab53b3a7ceea57e92bc6543119d94d
Mohd Naushad Ahmed <naushad.dln@gmail.com>
2021-10-13 22:43:28 +0530

    Draft commit

---
+++ sw/src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {MatBadgeModule} from '@angular/material/badge';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { SingleShipmentComponent } from './tracking/single-shipment/single-shipment.component';
+import { MultipleShipmentComponent } from './tracking/multiple-shipment/multiple-shipment.component';
+import { UploadComponent } from './batching/upload/upload.component';
+import { DownloadComponent } from './batching/download/download.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    SingleShipmentComponent,
+    MultipleShipmentComponent,
+    UploadComponent,
+    DownloadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    DragDropModule,
+    MatBadgeModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
